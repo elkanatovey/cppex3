53d47b1bdc4277e7350895af474ba3f893ae2070
elkanatovey <elkanatovey@gmail.com>
2019-01-15 22:11:00 +0200

    created class

--- Matrix.hpp
+++ Matrix.hpp
@@ -5,8 +5,21 @@
 #ifndef EX3CPP_MATRIX_HPP
 #define EX3CPP_MATRIX_HPP
 
+#include <vector>
 
+template <typename T>
 class Matrix {
+
+    std::vector<T> _matrixContainer;
+
+public:
+
+    Matrix(unsigned int rows, unsigned int columns);
+
+    Matrix(unsigned int rows, unsigned int columns, const std::vector<T>& cells);
+
+
+    ~Matrix()= default;
 
 };
 
