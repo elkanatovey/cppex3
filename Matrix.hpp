a61dc764be7e845401c9a85bba75b3db378c9668
elkanatovey <elkanatovey@gmail.com>
2019-01-15 21:48:34 +0200

    start!

---
+++ Matrix.hpp
@@ -0,0 +1,14 @@
+//
+// Created by elkanatovey on 1/15/19.
+//
+
+#ifndef EX3CPP_MATRIX_HPP
+#define EX3CPP_MATRIX_HPP
+
+
+class Matrix {
+
+};
+
+
+#endif //EX3CPP_MATRIX_HPP
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef EX3CPP_LIBRARY_H
+#define EX3CPP_LIBRARY_H
+
+void hello();
+
+#endif
